f2409cca711bf1dd8e093667196c702f54458177
mathmurdock <matheus.rodrisantos@bol.com.br>
2018-09-25 20:02:36 -0300

    Aula de liguagens formais de autonomos

---
+++ aula1-flex/teste.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	cout<<"Teste gedit C++ UBUNTU"<<endl; 
+	return 0; 
+}
---
+++ aula1-flex/teste1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int x=10;
+printf("\nx= %d",x);
+
+
+return(0);
+}
